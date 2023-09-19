9bcf1d30ee2fe207a0f5bb62fac90f70c44b3922
gerrishon ngugi <gerrishonngugi@gmail.com>
2023-09-19 03:39:42 -0700

    printf.c

---
+++ _printf.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * _printf - Produces output according to a format
+ * @format: Input string
+ *
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int count, i, tempcount;
+	char c;
+
+	i = count = tempcount = 0;
+
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				_putchar('%');
+				count++;
+			}
+			else
+			{
+				c = format[i];
+				tempcount = conversionspecifier(c, ap);
+				count += tempcount;
+			}
+		}
+		i++;
+	}
+	va_end(ap);
+	return (count);
+}
+
+/**
+ * conversionspecifier - Determines the appropriate character
+ * @c: Input character
+ * @ap: Variadic list
+ *
+ * Return: Number of characters printed
+ */
+int conversionspecifier(char c, va_list ap)
+{
+	if (c == 'c')
+		return (print_char(ap));
+	if (c == 's')
+		return (print_string(ap));
+	if (c == 'd' || c == 'i')
+		return (print_int(ap));
+	return (0);
+}
+

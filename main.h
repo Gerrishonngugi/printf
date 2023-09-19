6a19470b504a1ef29cf88b5d09d5229081cf68ed
gerrishon ngugi <gerrishonngugi@gmail.com>
2023-09-19 03:34:52 -0700

    main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdarg.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+int conversionspecifier(char, va_list);
+
+
+int print_char(va_list);
+int print_string(va_list);
+int print_int(va_list);
+void print_num(int);
+#endif
